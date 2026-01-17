536c61b237cdf8aa1ec9e768c053683965a67450
mdkawsar <info.mdkawsardev@gmail.com>
2026-01-17 16:42:38 +0600

    modified

--- static/js/portfolio.js
+++ static/js/portfolio.js
@@ -3,8 +3,12 @@
 let triger = false;
 let hint = document.querySelector('.hint');
 
+hint.addEventListener('click',()=> {
+    // document.querySelector('#c_hint').classList.add('d-none');
+    console.log("clicked")
+})
 hint.onmouseenter = () => {
-    document.querySelector('.showHint').style.visibility = "visible";
+    
 }
 hint.onmouseleave = () => {
     document.querySelector('.showHint').style.visibility = "hidden";
